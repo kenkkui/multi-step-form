6cb3616ffb3f34e05b12c18820a7934366a98da7
kenkkui <thifirstime@gmail.com>
2024-04-22 18:31:16 +0800

    add responsive design to all

--- src/components/Step 3/Checkbox.jsx
+++ src/components/Step 3/Checkbox.jsx
@@ -1,23 +1,52 @@
-import React from 'react';
+import React from "react";
 
-function Checkbox({ name, aside, price, isCardSelected, onClick }) {
+function Checkbox({
+  name,
+  aside,
+  price,
+  isCardSelected,
+  onClick,
+  amountPeriod,
+}) {
   return (
-    <div className={`card ${isCardSelected ? "active" : ""}`} onClick={() => onClick(prev => !prev)}>
-      <section className='check-name-wrapper'>
+    <div
+      className={`card ${isCardSelected ? "active" : ""}`}
+      onClick={() => onClick((prev) => !prev)}
+    >
+      <section className="check-name-wrapper">
         <div className="checkbox-wrapper">
           <label>
-            <input type="checkbox" checked={isCardSelected ? true : false} readOnly />
-            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="9" viewBox="0 0 12 9"><path fill="none" stroke="#FFF" strokeWidth="2" d="m1 4 3.433 3.433L10.866 1"/></svg>
+            <input
+              type="checkbox"
+              checked={isCardSelected ? true : false}
+              readOnly
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="9"
+              viewBox="0 0 12 9"
+            >
+              <path
+                fill="none"
+                stroke="#FFF"
+                strokeWidth="2"
+                d="m1 4 3.433 3.433L10.866 1"
+              />
+            </svg>
           </label>
         </div>
-        
+
         <div className="name-container">
           <h1>{name}</h1>
           <aside>{aside}</aside>
         </div>
       </section>
 
-      <div className="billing-price">+${price}</div>
+      <div className="billing-price">
+        +${price}
+        {amountPeriod}
+      </div>
     </div>
   );
 }
--- src/components/Step 3/StepThree.jsx
+++ src/components/Step 3/StepThree.jsx
@@ -7,46 +7,44 @@ import Checkbox from "./Checkbox";
 function StepThree({ nextStep, backStep, info, setInfo }) {
   function toggleCard(cardName, id) {
     if (info.step3.cardInfos.hasOwnProperty(cardName)) {
-      setInfo(prev => {
+      setInfo((prev) => {
         const updatedValues = {
           ...prev,
-          step3: { 
+          step3: {
             cardInfos: {
               ...prev.step3.cardInfos,
             },
-            selectedCards: info.step3.selectedCards.filter(item => item !== id)
-          }
-        }
+            selectedCards: info.step3.selectedCards.filter(
+              (item) => item !== id
+            ),
+          },
+        };
 
         delete updatedValues.step3.cardInfos[cardName];
         return updatedValues;
-      })
-      
+      });
     } else {
-      setInfo(prev => {
+      setInfo((prev) => {
         let price;
         if (info.step2.period === "monthly") {
-          price = CARDS[id].monthlyPrice
+          price = CARDS[id].monthlyPrice;
         } else {
           price = CARDS[id].yearlyPrice;
         }
 
-        const updatedValues ={
+        const updatedValues = {
           ...prev,
           step3: {
-            selectedCards: [
-              ...prev.step3.selectedCards,
-              id
-            ],
+            selectedCards: [...prev.step3.selectedCards, id],
             cardInfos: {
               ...prev.step3.cardInfos,
-              [cardName]: price
-            }
-          }
-        }
+              [cardName]: price,
+            },
+          },
+        };
 
-        return updatedValues
-      })
+        return updatedValues;
+      });
     }
   }
 
@@ -54,13 +52,13 @@ function StepThree({ nextStep, backStep, info, setInfo }) {
     for (let key of info.step3.selectedCards) {
       console.log(key);
 
-      setInfo(prev => {
+      setInfo((prev) => {
         let price;
-  
+
         if (info.step2.period === "monthly") {
-          price = CARDS[key].monthlyPrice
+          price = CARDS[key].monthlyPrice;
         } else {
-          price = CARDS[key].yearlyPrice
+          price = CARDS[key].yearlyPrice;
         }
 
         const updatedValues = {
@@ -69,58 +67,61 @@ function StepThree({ nextStep, backStep, info, setInfo }) {
             ...prev.step3,
             cardInfos: {
               ...prev.step3.cardInfos,
-              [CARDS[key].name]: price
-            }
-          }
-        }
+              [CARDS[key].name]: price,
+            },
+          },
+        };
 
-        return updatedValues
-      })
+        return updatedValues;
+      });
     }
-  }, [info.step2.period])
+  }, [info.step2.period]);
 
   const CARDS = [
     {
       id: 0,
-      name:"Online service",
-      aside:"Access to multiplayer gamers",
+      name: "Online service",
+      aside: "Access to multiplayer gamers",
       monthlyPrice: 1,
-      yearlyPrice: 10
+      yearlyPrice: 10,
     },
     {
       id: 1,
-      name:"Larger storage",
-      aside:"Extra 1TB of cloud save",
+      name: "Larger storage",
+      aside: "Extra 1TB of cloud save",
       monthlyPrice: 2,
-      yearlyPrice: 20
+      yearlyPrice: 20,
     },
     {
       id: 2,
-      name:"Customizable profile",
-      aside:"Custom theme on your profile",
+      name: "Customizable profile",
+      aside: "Custom theme on your profile",
       monthlyPrice: 2,
-      yearlyPrice: 20
-    }
-  ]
+      yearlyPrice: 20,
+    },
+  ];
 
   return (
     <main id="step-three">
       <section className="card-wrapper">
-        {CARDS.map(card => {
-          const periodCheck = info.step2.period === "monthly" ? card.monthlyPrice : card.yearlyPrice
+        {CARDS.map((card) => {
+          const periodCheck =
+            info.step2.period === "monthly"
+              ? card.monthlyPrice
+              : card.yearlyPrice;
+          const amountPeriod = info.step2.period === "monthly" ? "/mo" : "/yr";
 
           return (
             <Checkbox
               key={card.id}
-
               name={card.name}
               aside={card.aside}
               price={periodCheck}
-
+              amountPeriod={amountPeriod}
               onClick={() => toggleCard(card.name, card.id)}
               isCardSelected={info.step3.cardInfos.hasOwnProperty(card.name)}
             />
-          )
+          );
         })}
       </section>
 
